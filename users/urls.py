90498122fd58b90c4150c5cd6156ca296bbb160a
Dmitry <k1791@mail.ru>
2023-06-13 20:14:47 +0500

    task16/lesson 22 logout

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
+from django.urls import path
+from .views import *
+
+from users.apps import UsersConfig
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('', LoginView.as_view(template_name='users/login.html'), name="login"),
+    ]
+
+
